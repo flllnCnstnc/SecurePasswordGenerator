aa7190ee4fe6257c2300e775a100a1b17c61543e
coflu <cofluellen@gmail.com>
2022-08-18 19:02:00 -0500

    The eventListener was added along with the first prompt

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generateBtn();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Event listener
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", function() {
+    prompt('How many characters do you want in your password?')
+    
+})
